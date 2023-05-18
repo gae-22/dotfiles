f1e1a9ccc1231a7402472c47e383c6e098d8e7bc
gae <gaedev22@gmail.com>
2023-05-18 13:23:03 +0900

    customize the 'Guezwhoz'color theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,9 +11,6 @@ end)
 
 local config = {
     adjust_window_size_when_changing_font_size = false,
-    -- color_scheme = 'iceberg-dark',
-    color_scheme = 'Guezwhoz',
-    -- color_scheme = 'idleToes',
     enable_scroll_bar = true,
     font = wezterm.font('PleckJP'),
     font_size = 18.5,
@@ -35,48 +32,75 @@ local config = {
             {Text=' ' .. tab.active_pane.title .. ' '},
         }
     end),
+
+    -- customized 'Guezwhoz' color scheme following 'https://wezfurlong.org/wezterm/config/appearance.html'
     colors = {
-        cursor_bg = '#fffaf5',
-        cursor_fg = 'black',
-        cursor_border = '#fffaf5',
-        tab_bar = {
-            background = '#1b1f2f',
-            active_tab = {
-                bg_color = '#444b71',
-                fg_color = '#c6c8d1',
-                intensity = 'Normal',
-                underline = 'None',
-                italic = false,
-                strikethrough = false,
-            },
-            inactive_tab = {
-                bg_color = '#282d3e',
-                fg_color = '#c6c8d1',
-                intensity = 'Normal',
-                underline = 'None',
-                italic = false,
-                strikethrough = false,
-            },
-            inactive_tab_hover = {
-                bg_color = '#1b1f2f',
-                fg_color = '#c6c8d1',
-                intensity = 'Normal',
-                underline = 'None',
-                italic = true,
-            strikethrough = false,
-            },
-            new_tab = {
-                bg_color = '#1b1f2f',
-                fg_color = '#c6c8d1',
-                italic = false
-            },
-            new_tab_hover = {
-                bg_color = '#444b71',
-                fg_color = '#c6c8d1',
-                italic = false
-            },
-        },
+        -- The default text color
+        -- foreground = 'silver',
+        foreground = '#c0c0c0',
+        -- The default background color
+        -- background = 'black',
+        background = '#1c1c1c',
+
+        -- Overrides the cell background color when the current cell is occupied by the
+        -- cursor and the cursor style is set to Block
+        -- cursor_bg = '#52ad70',
+        cursor_bg = '#eeeeee',
+        -- Overrides the text color when the current cell is occupied by the cursor
+        -- cursor_fg = 'black',
+        cursor_fg = '#eeeeee',
+        -- Specifies the border color of the cursor when the cursor style is set to Block,
+        -- or the color of the vertical or horizontal bar when the cursor style is set to
+        -- Bar or Underline.
+        -- cursor_border = '#52ad70',
+        cursor_border = '#eeeeee',
+
+        -- the foreground color of selected text
+        -- selection_fg = 'black',
+        selection_fg = '#eeeeee',
+        -- the background color of selected text
+        -- selection_bg = '#fffacd',
+        selection_bg = '#005f5f',
+
+        -- The color of the scrollbar 'thumb'; the portion that represents the current viewport
+        scrollbar_thumb = '#222222',
+
+        -- The color of the split lines between panes
+        split = '#444444',
+
+
+        ansi = {'#080808','#ff5f5f','#87d7af','#d7d787','#5fafd7','#afafff','#77c7c7','#dadada'},
+        brights = {'#8a8a8a','#d75f5f','#afd7af','#bebe93','#87afd7','#afafd7','#67b7b7','#dadada'},
+        -- Arbitrary colors of the palette in the range from 16 to 255
+        indexed = { [136] = '#af8700' },
+
+        -- Since: 20220319-142410-0fcdea07
+        -- When the IME, a dead key or a leader key are being processed and are effectively
+        -- holding input pending the result of input composition, change the cursor
+        -- to this color to give a visual cue about the compose state.
+        compose_cursor = 'orange',
+
+        -- Colors for copy_mode and quick_select
+        -- available since: 20220807-113146-c2fee766
+        -- In copy_mode, the color of the active text is:
+        -- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
+        -- 2. selection_* otherwise
+        copy_mode_active_highlight_bg = { Color = '#000000' },
+
+        -- use `AnsiColor` to specify one of the ansi color palette values
+        -- (index 0-15) using one of the names 'Black', 'Maroon', 'Green',
+        --  'Olive', 'Navy', 'Purple', 'Teal', 'Silver', 'Grey', 'Red', 'Lime',
+        -- 'Yellow', 'Blue', 'Fuchsia', 'Aqua' or 'White'.
+        copy_mode_active_highlight_fg = { AnsiColor = 'Black' },
+        copy_mode_inactive_highlight_bg = { Color = '#52ad70' },
+        copy_mode_inactive_highlight_fg = { AnsiColor = 'White' },
+
+        quick_select_label_bg = { Color = 'peru' },
+        quick_select_label_fg = { Color = '#ffffff' },
+        quick_select_match_bg = { AnsiColor = 'Navy' },
+        quick_select_match_fg = { Color = '#ffffff' },
     },
+
     keys = {
         -- increase font size
         { key = '+', mods = 'CMD',       action = act.IncreaseFontSize },
