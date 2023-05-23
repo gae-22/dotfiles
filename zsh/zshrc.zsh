4a4994fe25f1bde068317d5de5a5e286051cc708
gae <gaedev22@gmail.com>
2023-05-23 15:16:41 +0900

    upgrade

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,17 +1,50 @@
+# ------------------------------
+# General Settings
+# ------------------------------
+export EDITOR=vim        # set "vim" as default editor
+export LANG=ja_JP.UTF-8  # set language
+export KCODE=u           # set encoding
+export AUTOFEATURE=true  # automatically generate feature tests
+
+autoload -U compinit
+compinit
+zstyle ':completion:*:default' menu select=1
+
+setopt correct           # correct mistyped commands
+setopt prompt_subst      # enable command substitution in prompt
+setopt notify            # report status of background jobs immediately
+
+### Complement ###
+autoload -U compinit; compinit # enable interpolation functionality.
+setopt auto_list               # display list automatically in case of multiple candidates
+setopt auto_menu               # display menu automatically in case of multiple candidates
+setopt list_packed             # display candidates in one line
+setopt list_types              # display file types in candidates
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' # case insensitive completion
+
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
     source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
 [[ -f "$HOME/.fig/shell/zshrc.pre.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.pre.zsh"
 
 export ZSH="$HOME/.oh-my-zsh"
+
+export PATH="/opt/homebrew/bin:$PATH"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+
+# themes
 # ZSH_THEME="original"
 # ZSH_THEME='robbyrussell'
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
 
 plugins=(
     git
@@ -22,13 +55,37 @@ plugins=(
 source $ZSH/oh-my-zsh.sh
 source $HOME/.zsh/git-prompt.sh
 
-alias ss="wezterm ssh sol"
-alias sn="wezterm ssh nest"
+# ------------------------------
+# History
+# ------------------------------
+HISTFILE=~/.zsh_history   # the file where histories are saved
+HISTSIZE=10000            # the number of history entries to save in a session
+SAVEHIST=10000            # the number of history entries to save in a history file
+setopt extended_history   # save timestamp of command in HISTFILE
+setopt hist_ignore_dups   # ignore duplication command history list
+setopt share_history      # share command history data with the other sessions
+setopt hist_reduce_blanks # remove superfluous blanks before recording entry
 
-alias vi="vim"
-alias ls="exa --icons"
-alias la="ls -a"
+# ------------------------------
+# Alias
+# ------------------------------
 
+# display history with timestamp
+alias history='history -E'
+
+# conduct "ls" after "cd"
+function cd() {
+    builtin cd $@ && ls;
+}
+
+# ssh
+alias ss="ssh sol"
+alias sn="ssh nest"
+
+# attatch icons to ls
+alias ls="lsd"
+
+# git
 alias gi ="git"
 alias gia="git add"
 alias gic="git commit -m"
@@ -37,17 +94,4 @@ alias gis="git status"
 alias gim="git merge"
 
 alias reload="source ~/.zshrc"
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-
-source ~/powerlevel10k/powerlevel10k.zsh-theme
-
-setopt share_history     # 同時に起動したzshで履歴を共有する
-setopt hist_ignore_space # 行頭にスペースがあるコマンドは履歴に残さない
-setopt hist_reduce_blanks # 余分なスペースは詰める
-setopt hist_verify       # 編集後のコマンドラインを確認する
-setopt inc_append_history # 既存の履歴に追加する
-setopt hist_expire_dups_first # 重複したコマンドのうち古い方を削除する
+alias vi="vim"
